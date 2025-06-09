0f33e97945ea502febe92be90e172b8e8a7ba6fa
Way2top <3066689820@qq.com>
2025-06-09 22:32:29 +0800

    2025.06.09

--- 力扣/代码随想录/数组/08_长度最小的子数组.py
+++ 力扣/代码随想录/数组/08_长度最小的子数组.py
@@ -1,5 +1,21 @@
 # Leetcode:209
 class Solution:
     def minSubArrayLen(self, target: int, nums: list[int]) -> int:
-        pass
-        
+        n = len(nums)
+        ans = 0
+        res = n
+        left = 0
+        count = 0
+        for right in range(n):
+            ans += nums[right]
+            while ans >= target:
+                res = min(res, right - left + 1)
+                ans -= nums[left]
+                left += 1
+                count += 1
+        if count == 0:
+            return 0
+        return res
+
+s = Solution()
+print(s.minSubArrayLen(15, [1,2,3,4,5]))
