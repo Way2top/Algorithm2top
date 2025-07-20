f00bb1e19f07ac43fbdbad19d85413d20de53636
Way2top <3066689820@qq.com>
2025-07-20 16:38:34 +0800

    111

---
+++ leetcode/2090.半径为-k-的子数组平均值.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=2090 lang=python3
+#
+# [2090] 半径为 k 的子数组平均值
+#
+
+# @lc code=start
+class Solution:
+    def getAverages(self, nums: list[int], k: int) -> list[int]:
+        n = len(nums)
+        
+        
+# @lc code=end
+
