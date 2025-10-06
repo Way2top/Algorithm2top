8cfe954d6fc213308989aeaa14db41341064d708
Way2top <3066689820@qq.com>
2025-10-06 16:42:24 +0800

    坚持

--- leetcode/common/283.py
+++ leetcode/common/283.py
@@ -3,6 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # 快慢指针
+        # 快慢指针，slow 代表需要的元素，fast 遍历元素
         slow = fast = 0 
-        
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+        for i in range(slow, len(nums)):
+            nums[i] = 0
